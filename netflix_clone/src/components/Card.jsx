5de5ce5d4652f0abb83e3f3786cf8fce5d7b71b5
CoolShivas <shivas2710cool00@gmail.com>
2024-03-16 10:06:12 +0530

    Implementation_of_cssStyling

--- netflix_clone/src/components/Card.jsx
+++ netflix_clone/src/components/Card.jsx
@@ -2,14 +2,14 @@
 const Card = (props) => {
     // console.log(props);
     return <>
-        <div className='cards'>
+        <div className='main_container'>
             <div className='card'>
                 <img src={props.imgSrc} alt="Image not found" className='card__img' />
                 <div className='card__info'>
-                    <span className='card__category'>
+                    <span className='card__span'>
                         {props.title}
                     </span>
-                    <h3 className='card__title'> {props.seriesName} </h3>
+                    <h3 className='card__heading'> {props.seriesName} </h3>
                     <a href={props.link} target='_blank'>
                         <button> Watch Now </button>
                     </a>
