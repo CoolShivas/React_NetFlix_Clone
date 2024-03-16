464fd07fc770aae9c19745606c23eeeafe718e9f
CoolShivas <shivas2710cool00@gmail.com>
2024-03-16 08:11:46 +0530

    Formation_of_Custom_Attributes

--- netflix_clone/src/main.jsx
+++ netflix_clone/src/main.jsx
@@ -7,13 +7,13 @@ function Card() {
   return <>
     <div className='cards'>
       <div className='card'>
-        <img src="https://wallpapercave.com/wp/wp4056410.jpg" alt="Image not found" className='card__img' />
+        <img src="" alt="Image not found" className='card__img' />
         <div className='card__info'>
           <span className='card__category'>
             A Netflix  Original Series
           </span>
-          <h3 className='card__title'> Dark </h3>
-          <a href="https://www.netflix.com/in/title/80990668?source=35" target='_blank'>
+          <h3 className='card__title'>  </h3>
+          <a href="" target='_blank'>
             <button> Watch Now </button>
           </a>
         </div>
@@ -25,5 +25,14 @@ function Card() {
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Card />
+  <>
+    <Card
+      imgSrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="Dark"
+      seriesName="Dark"
+      link="https://www.netflix.com/in/title/80990668?source=35"
+    />
+    <Card />
+    <Card />
+  </>,
 )
